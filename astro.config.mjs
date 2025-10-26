7d2841e87e12989bb77dabdd973a8d0e41c4bb25
takashi145 <t.shuhei0203@gmail.com>
2025-10-26 12:06:32 +0900

    base urlを修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,5 +15,5 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-	base: process.env.NODE_ENV === 'production' ? '/blog' : '/',
+	base: process.env.NODE_ENV === 'production' ? '/blog/' : '/',
 });
