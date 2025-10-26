cafeff93f570de5480a6d7ab928fad8a433899ce
takashi145 <t.shuhei0203@gmail.com>
2025-10-26 10:32:11 +0900

    サイトタイトル修正

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My Blog';
+export const SITE_TITLE = 'Takashi145 Blog';
 export const SITE_DESCRIPTION = 'Welcome to my blog!';
